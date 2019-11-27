6668871ab620b47b5f446c186df512afac810e18
jackey-qiu <crqiu2@gmail.com>
2019-11-27 16:26:50 +0100

    data viewer gui:now fully functional

--- scripts/Viewer/Data_Viewer_GUI.py
+++ scripts/Viewer/Data_Viewer_GUI.py
@@ -1,5 +1,7 @@
 import sys,os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
 from PyQt5 import uic
 import random
 import numpy as np
@@ -25,6 +27,21 @@ class MyMainWindow(QMainWindow):
         super(MyMainWindow, self).__init__(parent)
         uic.loadUi(os.path.join(DaFy_path,'scripts','Viewer','data_viewer.ui'),self)
         # self.setupUi(self)
+        # plt.style.use('ggplot')
+        matplotlib.rc('xtick', labelsize=10)
+        matplotlib.rc('ytick', labelsize=10)
+        plt.rcParams.update({'axes.labelsize': 10})
+        plt.rc('font',size = 10)
+        plt.rcParams['axes.linewidth'] = 1.5
+        plt.rcParams['xtick.major.size'] = 6
+        plt.rcParams['xtick.major.width'] = 2
+        plt.rcParams['xtick.minor.size'] = 4
+        plt.rcParams['xtick.minor.width'] = 1
+        plt.rcParams['ytick.major.size'] = 6
+        plt.rcParams['ytick.major.width'] = 2
+        plt.rcParams['ytick.minor.size'] = 4
+        plt.rcParams['ytick.minor.width'] = 1
+        plt.rcParams['mathtext.default']='regular'
         self.open.clicked.connect(self.load_file)
         self.plot.clicked.connect(self.plot_figure)
         self.apply.clicked.connect(self.replot_figure)
@@ -55,21 +72,90 @@ class MyMainWindow(QMainWindow):
         text = self.PushButton_fold_or_unfold.text()
         if text == "<":
             self.frame.setVisible(False)
-            self.fold_or_unfold.setText(">")
+            self.PushButton_fold_or_unfold.setText(">")
         elif text == ">":
             self.frame.setVisible(True)
-            self.fold_or_unfold.setText("<")
+            self.PushButton_fold_or_unfold.setText("<")
 
     def plot_figure(self):
-        # print(dir(self))
-        self.mplwidget.canvas.ax_img = self.mplwidget.canvas.figure.add_subplot(121)
-        self.mplwidget.canvas.ax_profile = self.mplwidget.canvas.figure.add_subplot(322)
-        self.mplwidget.canvas.ax_ctr = self.mplwidget.canvas.figure.add_subplot(324)
-        self.mplwidget.canvas.ax_pot = self.mplwidget.canvas.figure.add_subplot(326)
-        # self.mplwidget.canvas.axes = self.mplwidget.canvas.figure.add_subplot(111)
+        self.mplwidget.fig.clear()
+        plot_dim = [len(self.plot_labels_y), len(self.scans)]
+        for scan in self.scans:
+            setattr(self,'plot_axis_scan{}'.format(scan),[])
+            j = self.scans.index(scan) + 1
+            for i in range(plot_dim[0]):
+                getattr(self,'plot_axis_scan{}'.format(scan)).append(self.mplwidget.canvas.figure.add_subplot(plot_dim[0], plot_dim[1],j+plot_dim[1]*i))
+        y_max_values,y_min_values = [-100000000]*len(self.plot_labels_y),[100000000]*len(self.plot_labels_y)
+        for scan in self.scans:
+            for each in self.plot_labels_y:
+                i = self.plot_labels_y.index(each)
+                try:
+                    fmt = self.lineEdit_fmt.text().rsplit(',')[self.scans.index(scan)]
+                except:
+                    fmt = 'b-'
+                getattr(self,'plot_axis_scan{}'.format(scan))[i].plot(self.data_to_plot[scan][self.plot_label_x],self.data_to_plot[scan][self.plot_labels_y[i]],fmt)
+                if i==0:
+                    getattr(self,'plot_axis_scan{}'.format(scan))[i].set_title(r'pH {}_scan{}'.format(self.phs[self.scans.index(scan)],scan),fontsize=10)
+                temp_max, temp_min = max(list(self.data_to_plot[scan][self.plot_labels_y[i]])),min(list(self.data_to_plot[scan][self.plot_labels_y[i]]))
+                if y_max_values[i]<temp_max:
+                    y_max_values[i] = temp_max
+                if y_min_values[i]>temp_min:
+                    y_min_values[i] = temp_min
+                if i!=(len(self.plot_labels_y)-1):
+                    ax = getattr(self,'plot_axis_scan{}'.format(scan))[i]
+                    ax.set_xticklabels([])
+                else:
+                    ax = getattr(self,'plot_axis_scan{}'.format(scan))[i]
+                    x_label = [r'Image_no','E / V$_{RHE}$'][self.plot_label_x=='potential']
+                    ax.set_xlabel(x_label)
+                if scan!=self.scans[0]:
+                    getattr(self,'plot_axis_scan{}'.format(scan))[i].set_yticklabels([])
+                else:
+                    y_label_map = {'current':r'j / mAcm$^{-2}$',
+                                   'strain_ip':r'$\Delta\varepsilon_\parallel$  (%)',
+                                   'strain_oop':r'$\Delta\varepsilon_\perp$  (%)',
+                                   'grain_size_oop':r'$\Delta d_\perp$ / nm',
+                                   'grain_size_ip':r'$\Delta d_\parallel$ / nm',
+                                   'peak_intensity':r'Intensity / a.u.'}
+                    if each in y_label_map:
+                        getattr(self,'plot_axis_scan{}'.format(scan))[i].set_ylabel(y_label_map[each])
+                    else:
+                        pass
+        for scan in self.scans:
+            for each in self.plot_labels_y:
+                i = self.plot_labels_y.index(each)
+                getattr(self,'plot_axis_scan{}'.format(scan))[i].set_ylim(y_min_values[i],y_max_values[i])
+        # self.mplwidget.fig.tight_layout()
+        self.mplwidget.fig.subplots_adjust(wspace=0.04,hspace=0.04)
         self.mplwidget.canvas.draw()
+
     def replot_figure(self):
         pass
+
+    def prepare_data_to_plot(self,plot_label_list, scan_number):
+        scans_ = [int(each) for each in self.lineEdit_scan_numbers.text().rsplit(',')]
+        img_ranges_ = [[int(each_) for each_ in each.rsplit('-')] for each in self.lineEdit_point_ranges.text().rsplit(',')]
+        
+        if scan_number in scans_:
+            which = scans_.index(scan_number)
+            l , r = img_ranges_[which]
+        else:
+            l, r = 0, 100000000
+        if hasattr(self,'data_to_plot'):
+            self.data_to_plot[scan_number] = {}
+        else:
+            self.data_to_plot = {}
+            self.data_to_plot[scan_number] = {}
+
+        for each in plot_label_list:
+            if each=='potential':#RHE potential
+                self.data_to_plot[scan_number][each] = 0.205+np.array(self.data[self.data['scan_no'] == scan_number][each])[l:r]+0.059*np.array(self.data[self.data['scan_no'] == scan_number]['phs'])[l:r]
+            else:
+                if each in ['peak_intensity','peak_intensity_error','strain_ip','strain_oop','grain_size_ip','grain_size_oop']:
+                    temp_data = np.array(self.data[self.data['scan_no'] == scan_number][each])[l:r]
+                    self.data_to_plot[scan_number][each] = list(temp_data-max(temp_data))
+                else:
+                    self.data_to_plot[scan_number][each] = list(self.data[self.data['scan_no'] == scan_number][each])[l:r]
 
     def append_scans(self):
         text = self.scan_numbers_append.text()
@@ -81,6 +167,17 @@ class MyMainWindow(QMainWindow):
         scans = list(set([int(each) for each in text_new.rstrip().rsplit(',')]))
         scans.sort()
         self.scan_numbers_all.setText(','.join([str(scan) for scan in scans]))
+        assert (self.lineEdit_x.text()!='' and self.lineEdit_y.text()!=''), 'No channels for plotting have been selected!'
+        assert self.scan_numbers_all.text()!='', 'No scans have been selected!'
+        plot_labels = self.lineEdit_x.text() + ',' + self.lineEdit_y.text()
+        plot_labels = plot_labels.rstrip().rsplit(',')
+        for scan in scans:
+            self.prepare_data_to_plot(plot_labels,scan)
+            print('Prepare data for scan {} now!'.format(scan))
+        self.scans = scans
+        self.plot_label_x = self.lineEdit_x.text()
+        self.plot_labels_y = self.lineEdit_y.text().rstrip().rsplit(',')
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
